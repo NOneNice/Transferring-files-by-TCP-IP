de61529108d945577adf4f206c9fadfea0aa3ed6
NOneNice <55196139+NOneNice@users.noreply.github.com>
2021-09-25 17:55:38 +0700

    Creat File Server

---
+++ Client.cpp
@@ -0,0 +1,78 @@
+﻿#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+
+#include <WinSock2.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <experimental/filesystem>
+#include <Windows.h>
+
+
+#pragma comment(lib,"ws2_32.lib")
+#pragma warning (disable: 4996)
+
+using namespace std;
+
+void recv_file(SOCKET* sock) {
+
+	char file_size_str[16]; // Так как принимаем в чарах, то и файл надо сделать строкой
+	char file_name[32];
+
+	recv(*sock, file_size_str, 16, 0); // Тут указываем максимальное длина в байтах файла может быть 9999999999999999
+	int file_size = atoi(file_size_str); // И как только мы приняли файл, то переводим из строки в инт
+	char* bytes = new char[file_size]; // Для принятия самого файла в байтак
+
+	recv(*sock, file_name, 32, 0);
+
+	fstream file;
+	file.open(file_name, ios_base::out | ios_base::binary);
+
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+
+	cout << "size: " << file_size << endl;
+	cout << "name: " << file_name << endl;
+
+	if (file.is_open()) {
+
+		recv(*sock, bytes, file_size, 0);
+		cout << "data: " << bytes << endl;
+		cout << "Time: " << st.wHour << ":" << st.wMinute << ":" << st.wSecond << endl;
+
+		file.write(bytes, file_size);
+		cout << "Save\n";
+	}
+	else
+		cout << "Error file open\n";
+
+	delete[] bytes;
+	file.close();
+
+}
+
+
+int main(int argc, char* argv[]) {
+	
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(80);
+	addr.sin_family = AF_INET;
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) == 0) { // Тут точно так же соеденяемся с клиентом по выше указаным данным
+		cout << "Connectied!\n";
+		recv_file(&Connection);
+	}
+	
+
+	system("pause");
+	return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,83 @@
+﻿#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+
+#include <WinSock2.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <experimental/filesystem>
+#include <Windows.h>
+
+
+#pragma comment(lib,"ws2_32.lib")
+#pragma warning (disable: 4996)
+
+using namespace std;
+
+
+void send_file(SOCKET* sock, const string& file_name) {
+
+	fstream file; //Открываем файл
+	file.open(file_name, ios_base::in | ios_base::binary);
+
+	if (file.is_open()) {
+		
+		int file_size = experimental::filesystem::file_size(file_name) + 1; //Узнаем вес файла
+		
+		char* bytes = new char[file_size]; // Выделяем масив для файла
+
+		file.read(bytes, file_size); // Читаем файл
+
+		SYSTEMTIME st;
+		GetLocalTime(&st);
+
+		cout << "size:" << file_size << endl;
+		cout << "name:" << file_name << endl;
+		cout << "data:" << bytes << endl;
+		cout << "Time: " << st.wHour << ":" << st.wMinute << ":" << st.wSecond << endl;
+
+		send(*sock, to_string(file_size).c_str(), 16, 0); 
+		send(*sock, file_name.c_str(), 32, 0);
+		send(*sock, bytes, file_size, 0);
+		delete[] bytes;
+	}
+	else
+		cout << "Error file open\n";
+		
+		file.close();
+}
+
+
+int main(int argc, char* argv[]) {
+	
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1); // Запрашиваем библиотеку
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { // Загружаем библеотеку 1: Запрашиваемая версия библеотки 2: Ссылка на структуру
+		cout << "Error" << endl; // Если библеотека загрузилась то мы перейдем дальше, если же нет то выйдем из функции мейн и выводим ошибку 
+		exit(1);
+	}
+
+		SOCKADDR_IN addr; // Структура для хранения адреса 
+		int sizofaddr = sizeof(addr); 
+		addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // индефекация адреса
+		addr.sin_port = htons(80); // Индефикация порта
+		addr.sin_family = AF_INET; // Семейство протоколов
+		// Что бы два компьютера смогли сооединиться надо что бы 1 слушал а другой принимал прослушку
+		SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);  // Тут обьявляем что мы будем соеденятся через интерент протоколы и что структура Slisten будет слушать
+		bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // Привязываем адрес к сокету, указываем на структуру SOCKADDER, размер структуры сокадер
+		listen(sListen, SOMAXCONN); // Тут будем слушать порт, и ожидать подключение 
+
+		SOCKET nemConnection; // Новый сокет что бы удерживать соеденение с клиентом 
+		nemConnection = accept(sListen, (SOCKADDR*)&addr, &sizofaddr); // Как только мы произвели подключение, все данные будут храниться тут
+
+		if (nemConnection != 0) {
+			cout << "Client connected\n";
+
+			string path;
+			cin >> path;
+			send_file(&nemConnection, path);
+		} 
+
+
+		system("pause");
+		return 0;
+}
